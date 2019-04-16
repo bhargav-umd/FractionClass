58433c739e4660b480dfcf4c1786eda6d764cabc
bhargav-umd <bhargav@umd.edu>
2019-04-16 02:26:40 -0400

    Added more definitions considering T + int

--- include/fraction.hpp
+++ include/fraction.hpp
@@ -6,14 +6,52 @@
 
 template <typename T> T greatestCommonDivisor(const T &, const T &);
 
+template <typename T> class fraction;
+
+template <typename T>
+fraction<T> operator+(const fraction<T> &first, const fraction<T> &second);
+template <typename T>
+fraction<T> operator-(const fraction<T> &first, const fraction<T> &second);
+template <typename T>
+fraction<T> operator*(const fraction<T> &first, const fraction<T> &second);
+template <typename T>
+fraction<T> operator/(const fraction<T> &first, const fraction<T> &second);
+
+template <typename T>
+fraction<T> operator==(const fraction<T> &first, const fraction<T> &second);
+template <typename T>
+fraction<T> operator!=(const fraction<T> &first, const fraction<T> &second);
+template <typename T>
+fraction<T> operator>(const fraction<T> &first, const fraction<T> &second);
+template <typename T>
+fraction<T> operator<(const fraction<T> &first, const fraction<T> &second);
+
+template <typename T>
+fraction<T> operator+(const fraction<T> &first, const T &second);
+template <typename T>
+fraction<T> operator-(const fraction<T> &first, const T &second);
+template <typename T>
+fraction<T> operator*(const fraction<T> &first, const T &second);
+template <typename T>
+fraction<T> operator/(const fraction<T> &first, const T &second);
+
+template <typename T>
+fraction<T> operator==(const fraction<T> &first, const T &second);
+template <typename T>
+fraction<T> operator!=(const fraction<T> &first, const T &second);
+template <typename T>
+fraction<T> operator>(const fraction<T> &first, const T &second);
+template <typename T>
+fraction<T> operator<(const fraction<T> &first, const T &second);
+
 template <typename T> class fraction {
 private:
   T numerator, denominator;
 
 public:
-  fraction();
-  fraction(T);
-  fraction(const T &numerator, const T &denominator);
+  fraction<T>();
+  fraction<T>(T);
+  fraction<T>(const T &numerator, const T &denominator);
 
   T getNumerator();
   T getDenominator();
@@ -28,8 +66,24 @@ public:
   friend bool operator>(const fraction &, const fraction &);
   friend bool operator<(const fraction &, const fraction &);
 
-  friend std::istream &operator>>(std::istream &, fraction &);
-  friend std::ostream operator<<(std::ostream &, const fraction &);
+  friend fraction operator+(const fraction &, const T &);
+  friend fraction operator-(const fraction &, const T &);
+  friend fraction operator*(const fraction &, const T &);
+  friend fraction operator/(const fraction &, const T &);
+  friend bool operator==(const fraction &, const T &);
+  friend bool operator!=(const fraction &, const T &);
+  friend bool operator>(const fraction &, const T &);
+  friend bool operator<(const fraction &, const T &);
 };
 
 // Definitions
+
+template <typename T>
+T greatestCommonDivisor(const T &numerator, const T &denominator) {
+  if (numerator % denominator == 0) {
+    return denominator;
+  }
+  return greatestCommonDivisor(denominator, numerator % denominator);
+}
+
+template <typename T> fraction<T>::fraction() { T numerator = 0; }
