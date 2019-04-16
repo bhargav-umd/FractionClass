4c8574861a28cbddd8857e8634002a208f50e206
bhargav-umd <bhargav@umd.edu>
2019-04-16 04:02:14 -0400

    added all basic functions definitions, overloading pending

--- include/fraction.hpp
+++ include/fraction.hpp
@@ -51,12 +51,14 @@ private:
 public:
   fraction<T>();
   fraction<T>(T);
-  fraction<T>(const T &numerator, const T &denominator);
+  fraction<T>(const T &, const T &);
 
   T getNumerator();
   T getDenominator();
 
   void display();
+  fraction simplifyFractions(fraction &);
+
   friend fraction operator+(const fraction &, const fraction &);
   friend fraction operator-(const fraction &, const fraction &);
   friend fraction operator*(const fraction &, const fraction &);
@@ -76,8 +78,6 @@ public:
   friend bool operator<(const fraction &, const T &);
 };
 
-// Definitions
-
 template <typename T>
 T greatestCommonDivisor(const T &numerator, const T &denominator) {
   if (numerator % denominator == 0) {
@@ -86,4 +86,41 @@ T greatestCommonDivisor(const T &numerator, const T &denominator) {
   return greatestCommonDivisor(denominator, numerator % denominator);
 }
 
-template <typename T> fraction<T>::fraction() { T numerator = 0; }
+template <typename T> fraction<T>::fraction() {
+  this->numerator = 0;
+  this->denominator = 1;
+}
+
+template <typename T> fraction<T>::fraction(T number) {
+  this->numerator = number;
+  this->denominator = 1;
+}
+template <typename T> fraction<T>::fraction(const T &numer, const T &denomin) {
+  if (denominator == 0) {
+    throw std::domain_error("denominator cannot be zero");
+  }
+  this->numerator = numer;
+  this->denominator = denomin;
+}
+
+template <typename T>
+fraction<T> fraction<T>::simplifyFractions(fraction &frac) {
+  if (frac.denominator == 0) {
+    throw std::domain_error("denominator cannot be zero");
+  }
+
+  T gcd = greatestCommonDivisor(frac.numerator, frac.denominator);
+  frac.numerator /= gcd;
+  frac.denominator /= gcd;
+
+  return frac;
+}
+
+template <typename T> T fraction<T>::getNumerator() { return this->numerator; }
+template <typename T> T fraction<T>::getDenominator() {
+  return this->denominator;
+}
+
+template <typename T> void fraction<T>::display() {
+  std::cout << this->numerator << "/" << this->denominator << std::endl;
+}
